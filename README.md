eb25ab23bd78634c96e2d89ef327f45b7eba500c
Ikram Patah <ikram.patah@rebilly.com>
2022-08-24 01:32:34 +0800

    Update basic pinia

--- README.md
+++ README.md
@@ -5,8 +5,5 @@ Opiniated vue 3 boilerplate, bundled using vite. Basic configuration:
 - pug + scss in SFCs.
 - eslint + prettier.
 - vueRouter 4.
+- Pinia.
 - Boostrap grid.
-
-### Todo:
-
-- Add pinia.
--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 import { createApp } from 'vue';
 import App from '@/App.vue';
 import router from '@/router';
+import pinia from '@/store/pinia';
 
 // Import styling
 import '@/style/style.scss';
 
 const app = createApp(App);
 
+app.use(pinia);
 app.use(router);
 app.mount('#app');
